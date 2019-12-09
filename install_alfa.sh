e537d2e7b704795542d0672471a10e60e98ca627
drh4kor <drh4kor@gmail.com>
2019-12-08 20:20:03 -0800

    removed extra step; make not needed. use scripts

--- install_alfa.sh
+++ install_alfa.sh
@@ -2,6 +2,5 @@
 apt remove realtek-rtl88xxau-dkms && apt purge realtek-rtl88xxau-dkms
 git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl8812au
-#make && make install
 ./dkms-install.sh
 exit
